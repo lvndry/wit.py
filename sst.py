b9347ed482437a50defa6a8228b872a4886fbb0b
lvndry <monga.4landry@gmail.com>
2018-04-10 14:55:29 +0200

    add output

--- sst.py
+++ sst.py
@@ -1,6 +1,7 @@
-import requests
-import json
 import config.config as conf
+import json
+import requests
+import time
 from Recorder import record_audio, read_audio
 
 # Wit speech API endpoint
@@ -8,6 +9,9 @@ API_ENDPOINT = 'https://api.wit.ai/speech'
 
 def has_attribute(data, attribute):
     return attribute in data and data[attribute] is not None
+
+def command_error():
+    print ("Unavailable command")
 
 def startRecognize(AUDIO_FILENAME, num_seconds = 5):
 
@@ -41,7 +45,7 @@ def startRecognize(AUDIO_FILENAME, num_seconds = 5):
 
 
 def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
-
+    start_time = time.time()
     # record audio of specified length in specified audio file
     record_audio(num_seconds, AUDIO_FILENAME)
 
@@ -60,11 +64,9 @@ def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
     # converting response content to JSON format
     data = json.loads(resp.content)
 
-    # get text from data
-    text = data['_text']
-
+    excTime = round(time.time() - start_time, 2)
     # return the text
-    return text, data
+    return data, excTime
 
 if __name__ == "__main__":
     while 1:
@@ -73,6 +75,24 @@ if __name__ == "__main__":
             ok = startRecognize('starter.wav', 2)
 
         print("What can I do for you ?")
-        text, data =  RecognizeSpeech('myspeech.wav', 4)
+
+        data, excTime =  RecognizeSpeech('myspeech.wav', 4)
         print("\nData: {}".format(data))
-        print("\nYou said: {}".format(text))
+
+        if has_attribute(data, '_text'):
+            print("\nYou said: {}".format(data['_text']))
+        url = "http://api.opentime.com/"
+
+        if has_attribute(data, 'entities'):
+            if has_attribute(data['entities'], 'intent'):
+                ressource = data['entities']['intent'][0]['value']
+                url += ressource + '/'
+            else:
+                command_error()
+
+            if has_attribute(data['entities'], 'contact'):
+                contact = data['entities']['contact'][0]['value']
+                url += contact
+        print('API call: `%s`' % url)
+
+        print("\nTime: %s seconds" % excTime)
